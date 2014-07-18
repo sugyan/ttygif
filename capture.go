78f23b1092ab2cb812099d68a4098e535a6f9bb3
sugyan <sugi1982@gmail.com>
2014-07-18 19:41:28 +0900

    update capture for retina display

--- capture.go
+++ capture.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 )
 
 // CaptureImage take a screen shot of terminal
@@ -36,11 +37,59 @@ func captureByScreencapture(path string) (fileType string, err error) {
 		return
 	}
 	// get screen capture
-	// TODO: resize image if high resolution (retina display)
 	err = exec.Command("screencapture", "-l", string(windowID), "-o", "-m", "-t", "png", path).Run()
 	if err != nil {
 		return
 	}
+	// resize image if high resolution (retina display)
+	getProperty := func(key string) (result float64, err error) {
+		sips := exec.Command("sips", "-g", key, path)
+		awk := exec.Command("awk", "/:/ { print $2 }")
+		sipsOut, err := sips.StdoutPipe()
+		if err != nil {
+			return
+		}
+		awk.Stdin = sipsOut
+		sips.Start()
+		output, err := awk.Output()
+		if err != nil {
+			return
+		}
+		str := string(output)
+		result, err = strconv.ParseFloat(str[:len(str)-1], 32)
+		if err != nil {
+			return
+		}
+		return result, nil
+	}
+	properties, err := func() (results map[string]float64, err error) {
+		results = make(map[string]float64)
+		for _, key := range []string{"pixelHeight", "pixelWidth", "dpiHeight", "dpiWidth"} {
+			var property float64
+			property, err = getProperty(key)
+			if err != nil {
+				return
+			}
+			results[key] = property
+		}
+		return results, nil
+	}()
+	if err != nil {
+		return
+	}
+	if properties["dpiHeight"] > 72.0 && properties["dpiWidth"] > 72.0 {
+		pixelHeight := int(properties["pixelHeight"] * 72.0 / properties["dpiHeight"])
+		pixelWidth := int(properties["pixelWidth"] * 72.0 / properties["dpiWidth"])
+		err = exec.Command("sips",
+			"-s", "dpiWidth", "72.0", "-s", "dpiHeight", "72.0",
+			"-z", strconv.Itoa(pixelHeight), strconv.Itoa(pixelWidth),
+			path,
+		).Run()
+		if err != nil {
+			return
+		}
+	}
+
 	return "png", nil
 }
 
